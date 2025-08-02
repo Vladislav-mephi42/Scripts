f7e7ea34920d3f5d6eef597212c3fb441fb65ba4
klianchenkov.va <klianchenkov.va@dozen.mephi.ru>
2025-08-02 21:12:47 +0300

    Change monitoring.sh

--- bash/monitoring_script/monitoring.sh
+++ bash/monitoring_script/monitoring.sh
@@ -1,31 +1,47 @@
 #!/usr/bin/bash
 
 
-
-echo "=== МОНИТОРИНГ СЕРВЕРА ==="
-echo "Дата: $(date)"
-echo "Имя сервера: $(hostname)"
-echo "Время работы: $(uptime -p)"
-echo ""
-
-echo "Загрузка CPU:"
-load=$(uptime | awk -F'load average: ' '{print $2}' | cut -d, -f1)
-echo "Средняя загрузка: $load"
-cores=$(grep -c ^processor /proc/cpuinfo)
-echo "Количество ядер: $cores"
-echo ""
-
-echo "Использование памяти:"
-free -h | grep -v "Swap" | awk '{print $1,$2,$3,$4}'
-echo ""
-free -h | grep "Swap" | awk '{print $1,$2,$3,$4}'
-echo ""
-
-echo "Использование дисков:"
-df -h | grep -v "tmpfs" | awk '{print $1,$3,$4,$5}'
-echo ""
+general_information(){
+    echo "=== SERVER MONITORING ==="
+    echo "Date: $(date)"
+    echo "Server name: $(hostname)"
+    echo "Work time: $(uptime -p)"
+    echo ""
+}
 
 
-echo "Сетевые интерфейсы:"
-ip -o addr show | grep -v "lo" | awk '{print $2,$4}'
-echo ""
+cpu_information(){
+    echo "CPU usaage:"
+    load=$(uptime | awk -F'load average: ' '{print $2}' | cut -d, -f1)
+    echo "Average load: $load"
+    cores=$(grep -c ^processor /proc/cpuinfo)
+    echo "Number of cores: $cores"
+    echo ""
+}
+
+memory_information(){
+    echo "Memory usage:"
+    free -h | grep -v "Swap" | awk '{print $1,$2,$3,$4}'
+    echo ""
+    free -h | grep "Swap" | awk '{print $1,$2,$3,$4}'
+    echo ""
+}
+
+discs_information(){
+    echo "Discs usage:"
+    df -h | grep -v "tmpfs" | awk '{print $1,$3,$4,$5}'
+    echo ""
+}
+
+network_intrefaces_information(){
+    echo "Network intrefaces:"
+    ip -o addr show | grep -v "lo" | awk '{print $2,$4}'
+    echo ""
+}
+
+
+general_information
+cpu_information
+memory_information
+discs_information
+network_intrefaces_information
